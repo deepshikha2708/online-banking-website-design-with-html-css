009b8d6c27e9164b3415c6f3a8c4d605a76e36ce
Shreyan Sarkar <ss0693@gmail.com>
2023-10-15 21:52:18 +0530

    Create help.js

---
+++ help.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get references to the card elements
+    const faqCard = document.getElementById('card1');
+    const issueCard = document.getElementById('card2');
+
+    // Add click event listeners to the cards
+    faqCard.addEventListener('click', function () {
+        window.location.href = 'faq.html'; // Redirect to the FAQ page
+    });
+
+    issueCard.addEventListener('click', function () {
+        window.location.href = 'issue.html'; // Redirect to the Issue page
+    });
+});
